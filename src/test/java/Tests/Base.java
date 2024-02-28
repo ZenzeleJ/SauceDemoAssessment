1fd700f0cb2fc2a6b117569952216a86c5a793f8
Zamantuli <ZamaXulu-at-104039976716>
2024-02-28 18:32:50 +0200

    Created Base Class

---
+++ src/test/java/Tests/Base.java
@@ -0,0 +1,32 @@
+package Tests;
+
+import Pages.HomePage;
+import Pages.LoginPage;
+import Utils.BrowserFactory;
+import Utils.ReadFromExcel;
+import Utils.TakeScreenshots;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.io.IOException;
+
+public class Base {
+
+    BrowserFactory browserFactory = new BrowserFactory();
+
+    final WebDriver driver = browserFactory.startBrowser("chRoMe", "https://www.saucedemo.com/");
+    LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
+    HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+
+    TakeScreenshots takeScreenshots = new TakeScreenshots();
+
+    ReadFromExcel readFromExcel;
+
+    {
+        try {
+            readFromExcel = new ReadFromExcel();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
